5a121cc90635e24e247ac66d2e9d327ba5d9ddcd
Axel Fayet <axel.fayet97@gmail.com>
2022-01-18 17:35:16 +0100

    Worked on how to display product data

--- P5-Dev-Web-Kanap/front/Js/script.js
+++ P5-Dev-Web-Kanap/front/Js/script.js
@@ -25,26 +25,35 @@ fetch("http://localhost:3000/api/products/")
 
 /* for products.title -> create <p></p> ?*/
 
+const itemsSection = document.querySelector("#items");
+const createAnchor = document.createElement("a");
+const createText = document.createElement("p");
 
 function displayProducts(products) {
-    class produit {
-        constructor(colors, id, name, price, imageUrl, description, altTxt) {
-            this.colors = colors;
-            this.id = id;
-            this.name = name;
-            this.price = price;
-            this.imageUrl = imageUrl;
-            this.description = description;
-            this.altTxt = altTxt;
-        }
+    // class produit {
+    //     constructor(colors, id, name, price, imageUrl, description, altTxt) {
+    //         this.colors = colors;
+    //         this.id = id;
+    //         this.name = name;
+    //         this.price = price;
+    //         this.imageUrl = imageUrl;
+    //         this.description = description;
+    //         this.altTxt = altTxt;
+    //     }
+    // }
+
+    // let newProduct = new produit(products.colors, products._id, products.name, products.price, products.imageUrl, products.description, products.altTxt);
+    console.log(products);
+    
+    for (let article in products) {
+        console.log(article);
+        let link = itemsSection.appendChild(createAnchor);
+        link.appendChild(createText).innerText = 'Bonjour'
     }
+    products.forEach(article => {
+        link.appendChild(createText).innerText += "Bonjour"
+    });
+    // itemsSection.appendChild(createAnchor).classList.add("item__content");
+    // const itemLink = document.getElementsByClassName("items__content");
 
-    let newProduct = new produit(products.colors, products._id, products.name, products.price, products.imageUrl, products.description, products.altTxt);
-
-    console.log(newProduct);
-
-    const itemsSection = document.querySelector("#items");
-    const createAnchor = document.createElement("a");
-    itemsSection.appendChild(createAnchor).classList.add("item__content");
-    const itemLink = document.getElementsByClassName("items__content");
 }
