3f3e47cb933d678b76dfc864828453b020d989ae
axelfayet97 <axel.gayet97@gmail.com>
2022-01-21 10:41:24 +0100

    Product structure OK / Commentend code

--- P5-Dev-Web-Kanap/front/Js/script.js
+++ P5-Dev-Web-Kanap/front/Js/script.js
@@ -1,75 +1,56 @@
 // Création d'une requête API pour demander l'ensemble des produits.
-
 fetch("http://localhost:3000/api/products/")
-    // get promise
     .then(res => {
+        // Récupération du résultat de la requête et conversion en objet json
         return res.json();
     })
-    //promise OK : get promise's value
     .then(value => {
+        //Promesse acceptée, récupération de ses valeurs
         for (var products of value) {
             displayProducts(products);
         };
     })
-    // if error then :
     .catch(err => {
-        console.log('Une erreur est survenue.')
+        // Si une erreur survient
+        console.log("Une erreur est survenue." + error);
     });
 
-
-
-// Création de de balises pour les produits
-
-const itemsSection = document.getElementById("items");
-const createArticle = document.createElement("article");
-const createh3Title = document.createElement("h3");
-createh3Title.classList.add("productName");
-const createImgTag = document.createElement("img");
-const createPTag = document.createElement("p");
-createPTag.classList.add("productDescription");
-
-
 // Fonction générant les produits trouvés dans l'API
+function displayProducts(product) {
 
-function displayProducts(products) {
+    // Création du lien dynamique 
+    const createLink = document.createElement("a");
+    createLink.setAttribute("href", `./product.html?id=${product._id}`);
 
-    //URL en fonction de "URLSearchParam"
-    
-    itemsSection.innerHTML += `<a href='./product.html?id=${products._id}'></a>`;
+    // Création de la balise article
+    const createArticle = document.createElement("article");
 
-    let articleBody = document.querySelector("#items a").appendChild(createArticle);
-    articleBody.appendChild(createImgTag).src = `${products.imageUrl}`;
-    articleBody.appendChild(createImgTag).alt = `${products.altTxt}`;
-    articleBody.appendChild(createh3Title).innerText = `${products.name}`;
-    articleBody.appendChild(createPTag).innerText = `${products.description}`;
+    // Création de la balise image
+    const createImgTag = document.createElement("img");
+    createImgTag.setAttribute("src", product.imageUrl);
+    createImgTag.setAttribute("alt", product.altTxt);
+
+    // Création de la balise titre
+    const createh3Title = document.createElement("h3");
+    createh3Title.classList.add("productName");
+    createh3Title.textContent = product.name;
+
+    // Création de la balise texte
+    const createPTag = document.createElement("p");
+    createPTag.classList.add("productDescription");
+    createPTag.textContent = product.description;
+
+    // Récupération du conteneur
+    const itemContainer = document.getElementById("items");
+
+    // Attribution du contenu à la balise article
+    createArticle.appendChild(createImgTag);
+    createArticle.appendChild(createh3Title);
+    createArticle.appendChild(createPTag);
+
+    // Assigner un lien à la balise article
+    createLink.appendChild(createArticle);
+
+    // Assigner la balise <a> au conteneur
+    itemContainer.appendChild(createLink);
 }
-
-
-//Déclaration d'un objet
-// class kanap {
-//     constructor(colors, id, name, price, imageUrl, description, altTxt) {
-//         this.colors = colors;
-//         this.id = id;
-//         this.name = name;
-//         this.price = price;
-//         this.imageUrl = imageUrl;
-//         this.description = description;
-//         this.altTxt = altTxt;
-//     }
-
-//     printDescription() {
-//         console.log(`Description : ${this.description}`);
-//     }
-
-// myProducts() {
-
-// }
-
-    // let canapes = new kanap(products.colors, products.id, products.name, products.price, products.imageUrl, products.description, products.altTxt);
-
-    // canapes.printDescription();
-    // canapes.myProducts();
-
-
-    // console.log("produit : " + products);
-
