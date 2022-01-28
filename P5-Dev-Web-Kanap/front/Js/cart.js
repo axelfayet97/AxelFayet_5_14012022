b7fd5c4b6c4f0abfc04129a45a7021ae34950ba8
axelfayet97 <axel.gayet97@gmail.com>
2022-01-28 14:19:16 +0100

    Added guidelines for cart script

---
+++ P5-Dev-Web-Kanap/front/Js/cart.js
@@ -0,0 +1,11 @@
+// Récupération des produits dans le localstorage : image, nom, couleur, quantité, suppression
+
+// Affichage récapitulatif produit : affichage des données produits
+
+// Gestion de la quantité
+
+// + modification du prix en fonction
+
+// Fonction pour retirer l'objet du panier
+
+// Récupération des champs données du client
