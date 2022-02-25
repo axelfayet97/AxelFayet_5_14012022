78d57c532aba040b671f38c436cefe749aa7c17b
axelfayet97 <axel.gayet97@gmail.com>
2022-02-25 19:55:17 +0100

    Started working on confirmation script

--- P5-Dev-Web-Kanap/front/Js/confirmation.js
+++ P5-Dev-Web-Kanap/front/Js/confirmation.js
@@ -1,3 +1,9 @@
 // Récupération du numéro de commande assigné au préalable à l'utilisateur
+// fetch POST pour récupérer l'identifiant de commande dans la réponse de cette requete
+
+
+// Redirection de l'utilisateur sur la page confirmation en passant l'id de commande dans l'url
 
 // Affichage du numéro de commande dans le champs prévu à cet effet
+
+// ATTENTION A NE PAS STOCKER LE NUMERO 
