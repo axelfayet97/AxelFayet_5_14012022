8bf90239abf48dde60ae2dbecac6abef60f90948
Axel Fayet <axel.fayet97@gmail.com>
2022-02-28 17:36:44 +0100

    Confirmation script OK

--- P5-Dev-Web-Kanap/front/Js/confirmation.js
+++ P5-Dev-Web-Kanap/front/Js/confirmation.js
@@ -1,9 +1,12 @@
-// Récupération du numéro de commande assigné au préalable à l'utilisateur
-// fetch POST pour récupérer l'identifiant de commande dans la réponse de cette requete
+// On vide le panier à la commande
+localStorage.removeItem("products");
 
-
-// Redirection de l'utilisateur sur la page confirmation en passant l'id de commande dans l'url
+// Fonction récupérant le numéro de commande à partir de l'url
+function getOrder() {
+    var url = window.location.href;
+    var productsOrder = new URL(url).searchParams.get("order");
+    return productsOrder;
+}
 
 // Affichage du numéro de commande dans le champs prévu à cet effet
-
-// ATTENTION A NE PAS STOCKER LE NUMERO 
+document.getElementById("orderId").textContent = getOrder();
